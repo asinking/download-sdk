04d676363f0493c96d1d8e9445a752c273177eb6
liuqiang <2498360338@qq.com>
2021-02-20 16:22:50 +0800

    [add]

--- src/Utils/SignUtil.php
+++ src/Utils/SignUtil.php
@@ -6,18 +6,18 @@ class SignUtil
 {
     /**
      * 计算签名
-     * @param array $data
-     * @param string $secret_key
+     * @param array $data   待发送的数据
+     * @param string $secret_key  秘钥串
      * @return string
      */
     public static function calculateSign(array $data,string $secret_key)
     {
-        $data = $data ?? [];
         ksort($data);
-        $buff = '';
+        $stringBuf = '';
         foreach ($data as $k => $v) {
-            $buff .= "{$k}={$v}&";
+            $stringBuf .= "{$k}={$v}&";
         }
-        return strtoupper(md5("{$buff}key={$secret_key}"));
+        $string = hash_hmac("sha256", $stringBuf, $secret_key);
+        return strtoupper($string);
     }
 }
