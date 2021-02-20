b282aeabab21c0b2b1c99f9a2b792353bf98c478
liuqiang <2498360338@qq.com>
2021-02-20 17:43:28 +0800

    [update]

--- src/Utils/SignUtil.php
+++ src/Utils/SignUtil.php
@@ -6,11 +6,11 @@ class SignUtil
 {
     /**
      * 计算签名
-     * @param array $data   待发送的数据
-     * @param string $secret_key  秘钥串
+     * @param array $data 待签名的参数
+     * @param string $secret_key 秘钥串
      * @return string
      */
-    public static function calculateSign(array $data,string $secret_key)
+    public static function calculateSign(array $data, string $secret_key)
     {
         ksort($data);
         $stringBuf = '';
@@ -19,5 +19,18 @@ class SignUtil
         }
         $string = hash_hmac("sha256", $stringBuf, $secret_key);
         return strtoupper($string);
+    }
+
+    /**
+     * 验证签名
+     * @param array $data 待签名的参数
+     * @param string $secret_key 秘钥串
+     * @param string $comp_sign  待比较的签名
+     * @return bool
+     */
+    public static function verifySign(array $data, string $secret_key, string $comp_sign)
+    {
+        $correctSign = self::calculateSign($data, $secret_key);
+        return $correctSign == $comp_sign;
     }
 }
