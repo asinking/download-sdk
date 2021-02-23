c6fe2175626dc43b8dab3112ae536aa645abccc6
liuqiang <2498360338@qq.com>
2021-02-23 17:09:42 +0800

    [update]

--- src/Constants/Constant.php
+++ src/Constants/Constant.php
@@ -13,22 +13,27 @@ class Constant
      * 创建报表任务
      */
     const URL_CREATE_REPORT = '/v1/report/createReport';
+
     /**
      * 创建定时报表任务
      */
     const URL_CREATE_TIMER_REPORT = '/v1/report/createTimerReport';
+
     /**
      * 编辑定时报表任务
      */
     const URL_EDIT_TIMER_REPORT = '/v1/report/editTimerReport';
+
     /**
      * 重试导出报表
      */
     const URL_RETRY_REPORT = '/v1/report/retryReportExport';
+
     /**
      * 删除报表
      */
     const URL_DEL_REPORT = '/v1/report/delReport';
+
     /**
      * 获取所有报表任务分组
      */
